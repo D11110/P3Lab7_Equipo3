9dae6e02c32ca1a8fc9d0d3b070b888a17cb72d8
D11110 <danielaugustoma@hotmail.com>
2020-09-04 13:36:21 -0600

    Menu

---
+++ Tablero.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "Alfil.cpp"
+#include "Caballo.cpp"
+#include "Peon.cpp"
+#include "Rey.cpp"
+#include "Torre.cpp"
+
+using namespace std;
+
+int main()
+{
+    bool isPlaying = true;
+    while (isPlaying)
+    {
+        std::cout << "Menu" << std::endl;
+        std::cout << "1. Jugar" << std::endl;
+        std::cout << "2. Salir" << std::endl;
+
+        int opcionIngresadaPJugador;
+        cin >> opcionIngresadaPJugador;
+
+        switch (opcionIngresadaPJugador)
+        {
+        case 1:
+            
+            break;
+        
+        default:
+            break;
+        }
+    }
+}
